1841e298dcb9b6ce4fb8c4bcc3f03c2b27b82ded
klh8mr <klh8mr@virginia.edu>
2016-09-24 13:45:24 -0400

    Update UrbanaData.R

--- UrbanaData.R
+++ UrbanaData.R
@@ -1,9 +1,13 @@
 library(readr)
+library(dplyr)
 
+# read in raw crime data
 urbana_data <- read_csv('Police_Incidents_Since_1988.csv')
 
+# string split the mapping address to extract the Lat and Long
 splt <-strsplit(urbana_data$`MAPPING ADDRESS`, "\n")
 
+# cycle through to map split string into data frame
 dftemp <- data.frame(matrix(ncol=1))
 for (i in 1:3)
 {
@@ -15,8 +19,10 @@ dftemp <- dftemp[2:4]
 dftemp$`Lat and Long` <-sub("\\(", "", dftemp$`Lat and Long`)
 dftemp$`Lat and Long` <-sub("\\)", "", dftemp$`Lat and Long`)
 
+# split Lat and Long on ","
 splt2 <- strsplit(as.character(dftemp$`Lat and Long`), ",")
 
+# cycle through to map split string into data frame
 dftemp2 <-data.frame(matrix(ncol=1))
 for (i in 1:2)
 {
@@ -26,9 +32,15 @@ for (i in 1:2)
 colnames(dftemp2) <- c("x", "Latitude", "Longitude")
 dftemp2 <- dftemp2[2:3]
 
+# cbind data together
 urbana_data <- cbind(urbana_data, dftemp, dftemp2)
 
+# remove lines with "NA" in Lat and Long
 urbana_data <- urbana_data[!(is.na(urbana_data$Lat.and.Long)),]
 
-write_csv(urbana_data, "UrbanaData_master.csv")
+# filter to include only crimes after 2004
+urbana_data <- filter(urbana_data, urbana_data$YEAR.OCCURRED > 2004)
+
+# write data to csv
+write_csv(urbana_data, "UrbanaData.csv")
 
